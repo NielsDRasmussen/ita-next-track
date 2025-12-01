b84f2b5c59521fbe6527a92bf6fb8b7d8acf6098
Casp8207 <eaa25caf@students.eaaa.dk>
2025-12-01 09:46:14 +0100

    update

--- frontend/4.HostPage/HostPage.js
+++ frontend/4.HostPage/HostPage.js
@@ -1,11 +1,25 @@
-// 1. Hent popup HTML og indsæt i siden
-fetch("../6.Requests/RequestPopup.html")
-    .then(res => res.text())
-    .then(html => {
-        document.getElementById("requestPopupContainer").innerHTML = html;
-    });
+// Get the modal
+var modal = document.getElementById("Confirm");
 
-// 2. Request Song knap åbner popup med en test-sang
-document.getElementById("requestSongBtn").addEventListener("click", () => {
-    showRequestPopup("Shape of You – Ed Sheeran");
-});
+// Get the button that opens the modal
+var btn = document.getElementById("RequestPopup");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
