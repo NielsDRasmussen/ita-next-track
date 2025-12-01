31a5d651cbc04ed9b0308322c9f7b3ac91a6eff8
Casp8207 <eaa25caf@students.eaaa.dk>
2025-12-01 13:41:29 +0100

    hej

--- frontend/4.HostPage/HostPage.js
+++ frontend/4.HostPage/HostPage.js
@@ -1,4 +1,32 @@
 // Get the modal
+let SongModal = document.getElementById("NewSong");
+
+// Get the button that opens the modal
+let AddBtn = document.getElementById("AddBtn");
+
+// Get the <span> element that closes the modal
+let NewSpan = document.getElementsByClassName("Exit")[0];
+
+// When the user clicks on the confirm button, open the modal
+AddBtn.onclick = function() {
+  SongModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+NewSpan.onclick = function() {
+  SongModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target === modal) {
+    SongModal.style.display = "none";
+  }
+}
+
+
+// Her starter koden for popu for de andre deltagere
+// Get the modal
 let modal = document.getElementById("RequestPopup");
 
 // Get the button that opens the modal
