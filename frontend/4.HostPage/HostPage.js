2136dd38739db49761b7dfb85e838cef1d8a4abe
Casp8207 <eaa25caf@students.eaaa.dk>
2025-12-01 11:04:51 +0100

    her

--- frontend/4.HostPage/HostPage.js
+++ frontend/4.HostPage/HostPage.js
@@ -1,8 +1,8 @@
 // Get the modal
-var modal = document.getElementById("Confirm");
+var RequestModal = document.getElementById("Confirm");
 
 // Get the button that opens the modal
-var btn = document.getElementById("RequestPopup");
+var Confirm = document.getElementById("Confirm");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
