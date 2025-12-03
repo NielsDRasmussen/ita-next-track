3239942b650719d6c2a55d41de1a5c1085dbb47a
NielsDRasmussen <niels.dissing.rasmussen@gmial.com>
2025-12-03 10:33:34 +0100

    knap

--- frontend/5.ParticipantPage/ParticipantPage.js
+++ frontend/5.ParticipantPage/ParticipantPage.js
@@ -6,3 +6,10 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+
+
+//Tilbage til forsiden
+document.querySelector('.round-btn.leave').addEventListener('click', () => {
+    window.location.href = '/1.FrontPage/FrontPage.html';
+});
+
