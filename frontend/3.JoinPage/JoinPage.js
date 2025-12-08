6def42f76b56683b716628f9682a71f6f835819c
NielsDRasmussen <niels.dissing.rasmussen@gmial.com>
2025-12-08 14:12:25 +0100

    mikkel

--- frontend/3.JoinPage/JoinPage.js
+++ frontend/3.JoinPage/JoinPage.js
@@ -1,8 +1,3 @@
-//Start jam knap 
-document.getElementById('startBtn').addEventListener('click', () => {
-    window.location.href = '/5.ParticipantPage/ParticipantPage.html';
-});
-
 //Tilbage til forsiden
 document.querySelector('.close-btn').addEventListener('click', () => {
     window.location.href = '/1.FrontPage/FrontPage.html';
